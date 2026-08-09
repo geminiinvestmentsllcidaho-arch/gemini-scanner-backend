a430875f4f916f8d319990dee8c15edd381c4015
Goran Tadic <geminiinvestmentsllcidaho@gmail.com>
2026-08-09 18:21:14 +0000

    cleanup: remove obsolete paper order status CLI wrapper

--- scripts/paper_order_readonly_status_dashboard_panel.mjs
+++
@@ -1,4 +0,0 @@
-import { buildPaperOrderReadonlyStatusDashboardPanel, writePaperOrderReadonlyStatusDashboardPanel } from "../src/scanner/paper_order_readonly_status_dashboard_panel.mjs";
-const report = buildPaperOrderReadonlyStatusDashboardPanel({ runsDir: "runs" });
-const reportFile = writePaperOrderReadonlyStatusDashboardPanel(report);
-console.log(JSON.stringify({ ...report, reportFile }, null, 2));
